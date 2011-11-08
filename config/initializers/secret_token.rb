e7da86e0bc6927954e770f50a222a8104e9fade2
Steven <steven.buss@gmail.com>
2011-11-07 21:51:18 -0800

    Created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aether::Application.config.secret_token = 'd564da8727869b4b9c0287cbe34150e62864a9c48b2581129d81ee99b131132256c29cc806ef2ede164a38b67a1a85ca203f25f6d89cd65e3d0a2cc58590cc09'
