0bab4f9bd2d427090626905dc991748991408020
Steven <steven.buss@gmail.com>
2011-11-07 22:42:38 -0800

    Created user model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  has_many :dashboards
+  has_many :songs, :through => :votes
+
+  validates :name,  :presence   => true,
+                    :length     => { :maximum => 50 }
+
+  acts_as_url :name
+  def to_param
+    url
+  end
+end
