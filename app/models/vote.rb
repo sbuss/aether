e46be699b57ca9af0b05db268249165499dca12c
Steven <steven.buss@gmail.com>
2011-11-10 22:45:43 -0800

    Added voting_round to votes model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,9 @@
 class Vote < ActiveRecord::Base
   belongs_to :songs
   belongs_to :users
+  belongs_to :voting_round
+
+  validates_presence_of :song_id
+  validates_presence_of :user_id
+  validates_presence_of :voting_round_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111111062546) do
+ActiveRecord::Schema.define(:version => 20111111064459) do
 
   create_table "dashboards", :force => true do |t|
     t.string   "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(:version => 20111111062546) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "voting_round_id"
   end
 
   create_table "voting_rounds", :force => true do |t|
